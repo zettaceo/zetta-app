7c0dfd61360984308def12b3f2a98daf567f8823
zettaceo <contact@zettaword.global>
2025-12-18 23:43:28 -0300

    Create index.ts

---
+++ components/bank/index.ts
@@ -0,0 +1,7 @@
+export { default as BalanceCard } from "./BalanceCard";
+export { default as TransactionList } from "./TransactionList";
+export { default as VaultBalance } from "./VaultBalance";
+export { default as AssetList } from "./AssetList";
+export { default as WalletHistory } from "./WalletHistory";
+export { default as SwapPanel } from "./SwapPanel";
+export { default as PixQR } from "./PixQR";
