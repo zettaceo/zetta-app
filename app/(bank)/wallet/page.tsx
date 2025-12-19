e3700361c48a7346cda693515e2fcb3be646351b
zettaceo <contact@zettaword.global>
2025-12-18 23:46:52 -0300

    Update page.tsx

--- app/(bank)/wallet/page.tsx
+++ app/(bank)/wallet/page.tsx
@@ -1,22 +1,9 @@
-import VaultBalance from "@/components/bank/VaultBalance";
-import AssetList from "@/components/bank/AssetList";
-import WalletHistory from "@/components/bank/WalletHistory";
-import { assets } from "@/modules/wallet/wallet.mock";
+import { VaultBalance } from "@/components/bank";
 
 export default function WalletPage() {
-  const fiat = assets.filter(a => a.type === "fiat");
-  const crypto = assets.filter(a => a.type === "crypto");
-
   return (
-    <div className="space-y-6">
+    <div>
       <VaultBalance />
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <AssetList title="FIAT ASSETS" assets={fiat} />
-        <AssetList title="CRYPTO ASSETS" assets={crypto} />
-      </div>
-
-      <WalletHistory />
     </div>
   );
 }
